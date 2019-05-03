8b685f8eab2254ac734222a60b55e9ede83fdae6
Yashom Dighe <yashom7@gmail.com>
2019-05-04 01:10:53 +0530

    Lexical Analyser

---
+++ lexanalysier.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+kw = []
+op = []
+sc = []
+idty = []
+keywords = "auto break case char const continue default do double else enum extern float for goto if int long register return short signed sizeof static struct switch typedef union unsigned void volatile while"
+kwList = keywords.split()
+operators = "+ - * / % ++ -- < = <= > >= == != && || ! ?: & | << >> ~ ^ += -= *= /= %= "
+oplist = operators.split()
+schar = "{ } ( ) $ @ "
+scharlist = schar.split()
+
+
+inputFile = open('/home/universe/input.txt', 'r' )
+tokens = inputFile.read().split()
+
+for token in tokens:
+    for keyword in kwList:
+        if keyword in token:
+            kw.append(keyword)
+    if token in oplist:
+        op.append(token)
+    elif token in scharlist:
+        sc.append(token)
+    else :
+        idty.append(token)
+        
+        
+
+print kw
+print "number of keywords is ", len(kw)
+print op
+print "number of operators is ", len(op)
+print sc
+print "number of special characters is ", len(sc)
+print idty
+print "number of identifiers is ", len(idty)
